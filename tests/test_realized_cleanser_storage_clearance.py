8067018d6fcb02c782cf2e5e636864f25015f10d
mlngaxri <mlngax2009@gmail.com>
2026-09-05 19:19:24 +1000

    Cell 4: reuse cleanser package clearance fixtures

--- tests/test_realized_cleanser_storage_clearance.py
+++ tests/test_realized_cleanser_storage_clearance.py
@@ -1,18 +1,16 @@
-from masck_one.authority import load_authority
-from masck_one.model import build_model
-from masck_one.realized_cleanser_storage import (
-    PACKAGE_CLEARANCE_RESERVATION_MM,
-    build_realized_cleanser_storage,
-)
+from masck_one.realized_cleanser_storage import PACKAGE_CLEARANCE_RESERVATION_MM
 
 
 def _distance(a, b) -> float:
     return float(a.val().distance(b.val()))
 
 
-def test_cleanser_material_package_clears_released_shell_and_other_controlled_packages():
-    model = build_model()
-    cleanser = build_realized_cleanser_storage(model.authority)
+def test_cleanser_material_package_clears_released_shell_and_other_controlled_packages(
+    cell4_model,
+    cell4_cleanser_storage,
+):
+    model = cell4_model
+    cleanser = cell4_cleanser_storage
 
     other_packages = (
         model.shell.solid,
@@ -30,9 +28,12 @@ def test_cleanser_material_package_clears_released_shell_and_other_controlled_pa
             assert _distance(cleanser_material, package) >= PACKAGE_CLEARANCE_RESERVATION_MM
 
 
-def test_cleanser_service_sweeps_clear_released_package_geometry_with_reserved_margin():
-    model = build_model()
-    cleanser = build_realized_cleanser_storage(model.authority)
+def test_cleanser_service_sweeps_clear_released_package_geometry_with_reserved_margin(
+    cell4_model,
+    cell4_cleanser_storage,
+):
+    model = cell4_model
+    cleanser = cell4_cleanser_storage
 
     other_packages = (
         model.shell.solid,
@@ -49,10 +50,12 @@ def test_cleanser_service_sweeps_clear_released_package_geometry_with_reserved_m
             assert _distance(service_sweep, package) >= PACKAGE_CLEARANCE_RESERVATION_MM
 
 
-def test_cleanser_external_service_reservations_do_not_collapse_into_fresh_water_package():
-    model = build_model()
-    cleanser = build_realized_cleanser_storage(load_authority())
-    fresh_water = model.water_reservoir_envelope.solid
+def test_cleanser_external_service_reservations_do_not_collapse_into_fresh_water_package(
+    cell4_model,
+    cell4_cleanser_storage,
+):
+    cleanser = cell4_cleanser_storage
+    fresh_water = cell4_model.water_reservoir_envelope.solid
 
     for reservation in (
         cleanser.refill_closure_reservation_solid,
